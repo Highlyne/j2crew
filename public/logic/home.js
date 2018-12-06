9dfd78a342ecf412d3d3dd946aa2bb2e3a15cbd8
Tamara Hall <thall2000@gmail.com>
2018-12-05 22:44:29 -0500

    working on header

--- public/logic/home.js
+++ public/logic/home.js
@@ -2,4 +2,7 @@ console.log(" I am connected");
 $(document).ready(function(){
     $('.parallax').parallax();
     $('.modal').modal();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true
+    });
   });
